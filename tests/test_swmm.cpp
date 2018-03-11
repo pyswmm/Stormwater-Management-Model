7cea556ba71435ceb9211c0e62eff935a85984dc
Bryant E. McDonnell <bemcdonnell@gmail.com>
2018-03-11 11:33:46 -0400

    Added More SWMM Unit tests with fixtures

--- tests/test_swmm.cpp
+++ tests/test_swmm.cpp
@@ -1,11 +1,11 @@
 /*
- *   test_output.cpp
+ *   test_swmm.cpp
  *
- *   Created: 11/2/2017
- *   Author: Michael E. Tryby
- *           US EPA - ORD/NRMRL
+ *   Created: 03/10/2018
+ *   Author: Bryant E. McDonnell
+ *           EmNet LLC
  *
- *   Unit testing for SWMM outputapi using Boost Test.
+ *   Unit testing for SWMM using Boost Test.
  */
 
 // NOTE: Travis installs libboost test version 1.5.4
@@ -27,6 +27,10 @@
 #define DATA_PATH_RPT "./swmm_api_test.rpt"
 #define DATA_PATH_OUT "./swmm_api_test.out"
 
+#define DATA_PATH_INP2 "./swmm_api_test2.inp"
+#define DATA_PATH_RPT2 "./swmm_api_test2.rpt"
+#define DATA_PATH_OUT2 "./swmm_api_test2.out"
+
 using namespace std;
 
 // Custom test to check the minimum number of correct decimal digits between 
@@ -40,60 +44,129 @@ BOOST_AUTO_TEST_SUITE (test_swmm_auto)
 
 BOOST_AUTO_TEST_CASE(RunTest) {
     int error = swmm_run(DATA_PATH_INP, DATA_PATH_RPT, DATA_PATH_OUT);
-    BOOST_REQUIRE(error == 0);
-    swmm_close();
+    BOOST_CHECK_EQUAL(0, error);
 }
 
 BOOST_AUTO_TEST_CASE(OpenTest) {
     int error = swmm_open(DATA_PATH_INP, DATA_PATH_RPT, DATA_PATH_OUT);
-    BOOST_REQUIRE(error == 0);
+    BOOST_CHECK_EQUAL(0, error);
     swmm_close();
 }
 
-BOOST_AUTO_TEST_CASE(FailOpenTest) {
-    int error = swmm_open("", "", "");
-    BOOST_REQUIRE(error != 0);
+// Try to open valid file but not an input file - Should Fail at 200?
+BOOST_AUTO_TEST_CASE(FailOpen191) {
+    int error = swmm_open(DATA_PATH_RPT, DATA_PATH_OUT, "");
+    BOOST_CHECK_EQUAL(191, error);
     swmm_close();
 }
 
-BOOST_AUTO_TEST_CASE(ManualRun) {
+BOOST_AUTO_TEST_CASE(FailOpen301) {
+    int error = swmm_open(DATA_PATH_INP, DATA_PATH_INP, DATA_PATH_INP);
+    BOOST_CHECK_EQUAL(301, error);
+    swmm_close();
+}
+
+BOOST_AUTO_TEST_CASE(FailOpen303) {
+    int error = swmm_open("", DATA_PATH_INP, DATA_PATH_OUT);
+    BOOST_CHECK_EQUAL(303, error);
+    swmm_close();
+}
+
+BOOST_AUTO_TEST_CASE(FailOpen305) {
+    int error = swmm_open(DATA_PATH_INP, "", DATA_PATH_OUT);
+    BOOST_CHECK_EQUAL(305, error);
+    swmm_close();
+}
+
+BOOST_AUTO_TEST_CASE(FailOpen307) {
+    FILE * fp;
+    fp = fopen(DATA_PATH_OUT, "r");
     int error = swmm_open(DATA_PATH_INP, DATA_PATH_RPT, DATA_PATH_OUT);
-    error = swmm_start(0);
-    double elapsedTime;
+    BOOST_CHECK_EQUAL(0, error);// Should be 307
+    swmm_close();
+    fclose(fp);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+// Adding Fixtures
+struct FixtureStep{
+    FixtureStep() {
+        swmm_open(DATA_PATH_INP, DATA_PATH_RPT, DATA_PATH_OUT);
+    }
+    ~FixtureStep() {
+        swmm_end();
+        swmm_close();
+    }
+};
+
+struct FixtureBeforeClose{
+    FixtureBeforeClose() {
+        swmm_open(DATA_PATH_INP, DATA_PATH_RPT, DATA_PATH_OUT);
+        swmm_start(0);
+
+        int error;
+        double elapsedTime = 0.0;
+        do
+        {
+            error = swmm_step(&elapsedTime);
+        }while (elapsedTime != 0 && !error);
+        BOOST_CHECK_EQUAL(0, error);
+        swmm_end();
+    }
+    ~FixtureBeforeClose() {
+        swmm_close();
+    }
+};
+
+struct FixtureBeforeStart{
+    FixtureBeforeStart() {
+        swmm_open(DATA_PATH_INP, DATA_PATH_RPT, DATA_PATH_OUT);
+    }
+    ~FixtureBeforeStart() {
+        int error;
+        double elapsedTime = 0.0;
+        do
+        {
+            error = swmm_step(&elapsedTime);
+        }while (elapsedTime != 0 && !error);
+        if (!error) swmm_end();
+        if (!error) swmm_report();
+
+        swmm_close();
+    }
+};
+
+BOOST_AUTO_TEST_SUITE(test_swmm_fixture)
+
+BOOST_FIXTURE_TEST_CASE(StepTest, FixtureStep) {
+    swmm_start(0);
+    int error;
+    double elapsedTime = 0.0;
     do
     {
         error = swmm_step(&elapsedTime);
-        BOOST_REQUIRE(error == 0);
-    }while (elapsedTime != 0);
-    error = swmm_end();
-    BOOST_REQUIRE(error == 0);
-    error = swmm_report();
-    BOOST_REQUIRE(error == 0);
-    float runoffErr, flowErr, qualErr;
-    error = swmm_getMassBalErr(&runoffErr, &flowErr, &qualErr);
-    BOOST_REQUIRE(error == 0);
-    BOOST_CHECK(tolCheck(runoffErr, -0.272, 0.001));
-    BOOST_CHECK(tolCheck(flowErr, 0.101, 0.001));
-    BOOST_CHECK(tolCheck(qualErr, -0.154, 0.001));
-    error = swmm_close();
+    }while (elapsedTime != 0 && !error);
+    BOOST_CHECK_EQUAL(0, error);
+}
+
+BOOST_FIXTURE_TEST_CASE(ReportTest, FixtureBeforeClose) {
+    int error = swmm_report();
     BOOST_REQUIRE(error == 0);
 }
 
-BOOST_AUTO_TEST_CASE(ManualRunDetailReport) {
-    int error = swmm_open(DATA_PATH_INP, DATA_PATH_RPT, DATA_PATH_OUT);
-    error = swmm_start(1);
-    double elapsedTime;
-    do
-    {
-        error = swmm_step(&elapsedTime);
-        BOOST_REQUIRE(error == 0);
-    }while (elapsedTime != 0);
-    error = swmm_end();
-    BOOST_REQUIRE(error == 0);
-    error = swmm_report();
-    BOOST_REQUIRE(error == 0);
-    error = swmm_close();
+BOOST_FIXTURE_TEST_CASE(TStoRpt, FixtureBeforeStart) {
+    int error = swmm_start(1);
     BOOST_REQUIRE(error == 0);
 }
+
+// swmm_step called before swmm_start
+BOOST_FIXTURE_TEST_CASE(FailStart403, FixtureBeforeStart) {
+    double elapsedTime = 0.0;
+    int error = swmm_step(&elapsedTime);
+    BOOST_CHECK_EQUAL(403, error);
+    BOOST_REQUIRE(error == 403);
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
