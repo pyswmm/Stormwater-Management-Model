7f094168d3061eed8dc6ceeb9bdae85d4b9d3b51
vtzatchkov <vtzatchkov@yahoo.com.mx>
2020-06-09 14:33:18 -0500

    Unit testing files for coupling

---
+++ Coupling_unit_testing/src/coupling.c
@@ -0,0 +1,470 @@
+//-----------------------------------------------------------------------------
+//   coupling.c
+//
+//   Project:  EPA SWMM5
+//   Version:  5.1
+//   Date:     03/20/14   (Build 5.1.001)
+//             09/15/14   (Build 5.1.007)
+//             04/02/15   (Build 5.1.008)
+//             08/05/15   (Build 5.1.010)
+//             09/06/20
+//   Authors:  Laurent Courty
+//             Velitchko G. Tzatchkov
+//
+//   Copyrighted (c) distributed under the MIT license (see LICENSE.txt)
+//
+//   Overland model flow coupling functions.
+//
+//-----------------------------------------------------------------------------
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "headers.h"
+
+//=============================================================================
+
+int opening_findCouplingType(double crestElev, double nodeHead, double overlandHead, 
+                             double overflowArea, double weirWidth)
+//
+//  Input:   nodeHead = water elevation in the node (ft)
+//           crestElev = elevation of the node crest (= ground) (ft)
+//           overlandHead = water elevation in the overland model (ft)
+//           ...
+//  Output:  Type of Coupling
+//  Purpose: determine the coupling type of an opening
+//           according the the relative water elevations in node and surface
+//
+{
+    int overflow, drainage;
+    int overflowOrifice, drainageOrifice, submergedWeir, freeWeir;
+    double surfaceDepth, weirRatio;
+ 
+    surfaceDepth = overlandHead - crestElev;
+    weirRatio = overflowArea / weirWidth;
+
+    // --- boolean cases. See DOI 10.1016/j.jhydrol.2017.06.024
+    overflow = nodeHead > overlandHead;
+    drainage = nodeHead < overlandHead;
+    overflowOrifice = overflow && (nodeHead > crestElev);    
+    drainageOrifice = drainage &&
+                        ((nodeHead > crestElev) && (surfaceDepth >= weirRatio));
+    submergedWeir = drainage &&
+                    ((nodeHead > crestElev) && (surfaceDepth < weirRatio));
+    freeWeir = drainage && (nodeHead < crestElev) && (overlandHead > crestElev);
+
+    // --- set the coupling type
+    if ( !overflow && !drainage ) return NO_COUPLING_FLOW;
+    else if ( overflowOrifice || drainageOrifice )
+        return ORIFICE_COUPLING;
+    else if ( submergedWeir ) return SUBMERGED_WEIR_COUPLING;
+    else if ( freeWeir ) return FREE_WEIR_COUPLING;
+    else return NO_COUPLING_FLOW;
+}
+
+double opening_findCouplingInflow(int couplingType, double crestElev,
+                                double nodeHead, double overlandHead, double orificeCoeff, 
+                                double freeWeirCoeff, double subWeirCoeff, double overflowArea, 
+                                double weirWidth)
+//
+//  Input:   nodeHead = water elevation in the node (ft)
+//           crestElev = elevation of the node crest (= ground) (ft)
+//           overlandHead = water elevation in the overland model (ft)
+//           ..
+//  Output:  the flow entering through the opening (ft3/s)
+//  Purpose: computes the coupling flow of the opening
+//
+{
+    double headUp, headDown, headDiff, depthUp;
+    double orif_v, sweir_v, u_couplingFlow;
+
+	double exponente= 1.5;
+	double sqrt2g = sqrt(2 * GRAVITY);
+
+    headUp = fmax(overlandHead, nodeHead);
+    headDown = fmin(overlandHead, nodeHead);
+    headDiff = headUp - headDown;
+    depthUp =  headUp - crestElev;
+
+    switch(couplingType)
+    {
+        case ORIFICE_COUPLING:
+            orif_v = sqrt(2 * GRAVITY * headDiff);
+            u_couplingFlow = orificeCoeff * overflowArea * orif_v;
+            break;
+        case FREE_WEIR_COUPLING:
+            u_couplingFlow = (2/3.) * freeWeirCoeff * weirWidth *
+                             pow(depthUp, exponente) * sqrt2g;
+            break;
+        case SUBMERGED_WEIR_COUPLING:
+            sweir_v = sqrt(2. * GRAVITY * depthUp);
+            u_couplingFlow = subWeirCoeff * weirWidth * depthUp * sweir_v;
+            break;
+        default: u_couplingFlow = 0.0; break;
+    }
+    // --- Flow into the node is positive
+    // opening->newInflow = copysign(u_couplingFlow, overlandHead - nodeHead);
+	if (overlandHead - nodeHead > 0)
+	{
+		// opening->newInflow = fabs(u_couplingFlow);
+        return fabs(u_couplingFlow);
+	} else
+	{
+		// opening->newInflow = -fabs(u_couplingFlow);
+		return  -fabs(u_couplingFlow);
+	} 
+}
+
+//=============================================================================
+void coupling_adjustInflows(TCoverOpening* opening, double inflowAdjustingFactor)
+//
+//  Input:   opening = ...
+//           inflowAdjustingFactor = an inflow adjusting coefficient
+//  Output:  none
+//  Purpose: adjust the inflow according to an adjusting factor
+//
+{
+    // --- iterate among the openings
+    while ( opening )
+    {
+        opening->newInflow = opening->newInflow * inflowAdjustingFactor;
+        opening = opening->next;
+    }
+}
+
+//=============================================================================
+
+double coupling_findNodeInflow(double tStep, double Node_invertElev, double Node_fullDepth, double Node_newDepth, double Node_overlandDepth, 
+							   TCoverOpening * opening, double Node_couplingArea)
+//
+//  Input:   tStep = time step of the drainage model (s)
+//           overlandDepth = water depth in the overland model (ft)
+//           overlandSurfArea = surface in the overland model that is coupled to this node (ft2)
+//           ...
+//  Output:  coupling inflow
+//  Purpose: compute the coupling inflow for each node's opening].overlandDepth
+//
+{
+    double crestElev, overlandHead, nodeHead;
+    double totalCouplingInflow;
+    double rawMaxInflow, maxInflow, inflowAdjustingFactor;
+    int inflow2outflow, outflow2inflow;
+	TCoverOpening* First_opening = opening;
+
+    // --- calculate elevations
+    crestElev = Node_invertElev + Node_fullDepth;
+    nodeHead = Node_invertElev + Node_newDepth;
+    overlandHead = crestElev + Node_overlandDepth;
+
+    // --- init
+    totalCouplingInflow = 0.0;
+
+    // --- iterate among the openings
+    while ( opening )
+    {
+        // --- do nothing if not coupled
+        if ( opening->couplingType == NO_COUPLING ) continue;
+        // --- compute types and inflows
+        opening->couplingType = opening_findCouplingType(crestElev, nodeHead, overlandHead, opening->area, opening->length);
+        opening->newInflow = opening_findCouplingInflow(opening->couplingType, crestElev, nodeHead, overlandHead,
+                                                        opening->coeffOrifice, opening->coeffFreeWeir, 
+                                                        opening->coeffSubWeir, opening->area, 
+                                                        opening->length);
+        // --- prevent oscillations
+        inflow2outflow = (opening->oldInflow > 0.0) && (opening->newInflow < 0.0);
+        outflow2inflow = (opening->oldInflow < 0.0) && (opening->newInflow > 0.0);
+        if (inflow2outflow || outflow2inflow)
+        {
+            opening->couplingType = NO_COUPLING_FLOW;
+            opening->newInflow = 0.0;
+        }
+        // --- add inflow to total inflow
+        totalCouplingInflow += opening->newInflow;
+        opening = opening->next;
+    }
+    // --- inflow cannot drain the overland model
+    if (totalCouplingInflow > 0.0)
+    {
+        rawMaxInflow = (Node_overlandDepth * Node_couplingArea) / tStep;
+        // maxInflow = fmin(rawMaxInflow, totalCouplingInflow);
+        maxInflow = fmin(rawMaxInflow, totalCouplingInflow);
+        inflowAdjustingFactor = maxInflow / totalCouplingInflow;
+        coupling_adjustInflows(First_opening, inflowAdjustingFactor);
+
+        // --- get adjusted inflows
+        while ( opening )
+        {
+            totalCouplingInflow += opening->newInflow;
+            opening = opening->next;
+        }
+    }
+    return totalCouplingInflow;
+}
+
+//=============================================================================
+
+void coupling_execute(double tStep)
+//
+//  Input:   tStep = time step of the drainage model (s)
+//  Output:  none
+//  Purpose: find coupling flow for each node.
+//
+{
+    int j;
+
+    for ( j = 0; j < Nobjects[NODE]; j++ )
+    {
+        if ( !coupling_isNodeCoupled(j) ) continue;
+		Node[j].couplingInflow = coupling_findNodeInflow(tStep, Node[j].invertElev, Node[j].fullDepth, Node[j].newDepth, Node[j].overlandDepth, 
+							   Node[j].coverOpening, Node[j].couplingArea);    }
+}
+
+//=============================================================================
+
+void coupling_setOldState(int j)
+//
+//  Input:   j = node index
+//  Output:  none
+//  Purpose: replaces a node's old hydraulic coupling state values with new ones.
+//
+{
+    TCoverOpening* opening;
+
+    // --- iterate among the openings.
+    opening = Node[j].coverOpening;
+    while ( opening )
+    {
+        opening->oldInflow = opening->newInflow;
+        opening = opening->next;
+    }
+}
+
+//=============================================================================
+
+int coupling_isNodeCoupled(int j)
+//
+//  Input:   j = node index
+//  Output:  returns the coupling status of a node (yes/no)
+//  Purpose: if at least one opening is coupled, return TRUE.
+//
+{
+    int isCoupled = FALSE;
+    TCoverOpening* opening;
+
+    // --- iterate among the openings. If one is coupled, return YES
+    opening = Node[j].coverOpening;
+    while ( opening )
+    {
+        if ( opening->couplingType != NO_COUPLING )
+        {
+            isCoupled = TRUE;
+            break;
+        }
+        opening = opening->next;
+    }
+    return isCoupled;
+}
+
+//=============================================================================
+
+int coupling_closeOpening(int j, int idx)
+//
+//  Input:   j = node index
+//           idx = opening index
+//  Output:  Error code
+//  Purpose: Close an opening
+//
+{
+    int errcode = 0;
+    TCoverOpening* opening;  // opening object
+
+    // --- Find the opening
+    opening = Node[j].coverOpening;
+    while ( opening )
+    {
+        if ( opening->ID == idx ) break;
+        opening = opening->next;
+    }
+    // --- if opening doesn't exist, return an error
+    if ( opening == NULL )
+    {
+        return(ERR_API_OBJECT_INDEX);
+    }
+
+    // --- Close the opening
+    opening->couplingType  = NO_COUPLING;
+    return 0;
+}
+
+//=============================================================================
+
+int coupling_openOpening(int j, int idx)
+//
+//  Input:   j = node index
+//           idx = opening index
+//  Output:  Error code
+//  Purpose: Open an opening
+//
+{
+    int errcode = 0;
+    TCoverOpening* opening;  // opening object
+
+    // --- Find the opening
+    opening = Node[j].coverOpening;
+    while ( opening )
+    {
+        if ( opening->ID == idx ) break;
+        opening = opening->next;
+    }
+    // --- if opening doesn't exist, return an error
+    if ( opening == NULL )
+    {
+        return(ERR_API_OBJECT_INDEX);
+    }
+
+    // --- Open the opening
+    opening->couplingType  = NO_COUPLING_FLOW;
+    return 0;
+}
+
+//=============================================================================
+
+int coupling_setOpening(int j, int idx, int oType, double A, double l, double Co, double Cfw, 
+                        double Csw)
+// Purpose: Assigns property values to the node opening object
+// Inputs:  j     = Node index
+//          idx   =  opening's index
+//          otype =  type of opening (grate, etc). From an enum
+//          A     = area of the opening (ft2)
+//          l     = length of the opening (~circumference, ft)
+//          Co    = orifice coefficient
+//          Cfw   = free weir coefficient
+//          Csw   = submerged weir coefficient
+// Return:  error code
+{
+    int errcode = 0;
+    TCoverOpening* opening;  // opening object
+
+    // --- check if an opening object with this index already exists
+    opening = Node[j].coverOpening;
+    while ( opening )
+    {
+        if ( opening->ID == idx ) break;
+        opening = opening->next;
+    }
+
+    // --- if it doesn't exist, then create it
+    if ( opening == NULL )
+    {
+        opening = (TCoverOpening *) malloc(sizeof(TCoverOpening));
+        if ( opening == NULL )
+        {
+            return error_setInpError(ERR_MEMORY, "");
+        }
+        opening->next = Node[j].coverOpening;
+        Node[j].coverOpening = opening;
+    }
+
+    // Assign values to the opening object
+    opening->ID            = idx;
+    opening->type          = oType;
+    opening->area          = A;
+    opening->length        = l;
+    opening->coeffOrifice  = Co;
+    opening->coeffFreeWeir = Cfw;
+    opening->coeffSubWeir  = Csw;
+    // --- default values
+    opening->couplingType  = NO_COUPLING_FLOW;
+    opening->oldInflow     = 0.0;
+    opening->newInflow     = 0.0;
+
+    return(errcode);
+}
+
+//=============================================================================
+
+int coupling_countOpenings(int j)
+//
+//  Input:   j = node index
+//  Output:  number of openings in the node
+//  Purpose: count the number of openings a node have.
+//
+{
+    int openingCounter = 0;
+    TCoverOpening* opening;
+
+    opening = Node[j].coverOpening;
+    while ( opening )
+    {
+        if ( opening ) openingCounter++;
+        opening = opening->next;
+    }
+    return(openingCounter);
+}
+
+//=============================================================================
+
+int coupling_deleteOpening(int j, int idx)
+//
+//  Input:   j = node index
+//           idx = opening index
+//  Output:  none
+//  Purpose: delete an opening from a node.
+//
+{
+    // If position to deleete is negative 
+    if (idx < 0) return -1; //Bad idx - no opening to delete
+    TCoverOpening* opening;
+    opening = Node[j].coverOpening;
+    if (opening == NULL) return 0; //No opening to delete 
+    // Store first opening
+    TCoverOpening* temp = opening; 
+    // If the first opening needs to be deleted 
+    if (idx == 0) 
+    { 
+        Node[j].coverOpening = temp->next;   // Change pointer to the second opening
+        free(temp);                          // free former first opening
+        return 1; 
+    } 
+
+    // Find previous opening of the opening to be deleted 
+    for (int i=0; temp!=NULL && i<idx-1; i++) 
+         temp = temp->next; 
+  
+    // Position is more than number of openings 
+    if (temp == NULL || temp->next == NULL) return -2; //Bad idx - no opening to delete
+  
+    // Opening temp->next is the opening to be deleted 
+    // Store pointer to the next of opening to be deleted 
+    TCoverOpening* next = temp->next->next; 
+  
+    // Unlink the opening from the linked list 
+    free(temp->next);  // Free memory 
+  
+    temp->next = next;  // Unlink the deleted node from list 
+    return idx+1;
+}
+
+//=============================================================================
+
+void coupling_deleteOpenings(int j)
+//
+//  Input:   j = node index
+//  Output:  none
+//  Purpose: deletes all opening data for a node.
+//
+{
+    TCoverOpening* opening1;
+    TCoverOpening* opening2;
+    opening1 = Node[j].coverOpening;
+    while ( opening1 )
+    {
+        opening2 = opening1->next;
+        free(opening1);
+        opening1 = opening2;
+    }
+}
+
+//=============================================================================
+
---
+++ Coupling_unit_testing/tests/test_coupling.cpp
@@ -0,0 +1,52 @@
+/*
+ *   test_coupling.cpp
+ *
+ *   Created: 09/06/2020
+ *   Author: Velitchko G. Tzatchkov
+ *           Laurent Courty
+ *           IMTA
+ *
+ *   Unit testing for SWMM coupling using Boost Test.
+ */
+
+#define BOOST_TEST_MODULE "coupling"
+#include <boost/test/included/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include "headers.h"
+#include "coupling.h"
+
+// The following two declarations are required by coupling.c to work
+TNode* Node = new TNode[2];
+int Nobjects[MAX_OBJ_TYPES];
+
+int couplingType;
+
+// Test number             0     1     2     3     4     5     6     7     8     9     10
+double crestElev[11] =    {2.0,  2.0,  2.0,  2.0,  2.0,  3.0,  2.0,  2.0,  2.0,  2.0, 2.0};
+double nodeHead[11] =     {0.0,  2.0,  3.0,  4.0,  1.0,  2.0,  1.0,  3.0,  3.0,  3.0, 1.0};
+double overlandHead[11] = {2.0,  2.0,  2.0,  3.0,  1.0,  1.0,  3.0,  4.0,  3.5,  5.0, 2.0};
+double overflowArea[11] = {25.0, 25.0, 25.0, 25.0, 25.0, 25.0, 10.0, 10.0, 10.0, 10.0, 10.0};
+double weirWidth[11] =    {1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  5.0,  5.0,  5.0,  5.0, 5.0};
+int ExpectedType[11] = {NO_COUPLING_FLOW, NO_COUPLING_FLOW, ORIFICE_COUPLING, ORIFICE_COUPLING,
+                        NO_COUPLING_FLOW, NO_COUPLING_FLOW, FREE_WEIR_COUPLING, ORIFICE_COUPLING,
+                        SUBMERGED_WEIR_COUPLING, ORIFICE_COUPLING, NO_COUPLING_FLOW};
+        
+BOOST_AUTO_TEST_SUITE(test_coupling)
+
+    BOOST_DATA_TEST_CASE(ParameterizedCouplingTest, boost::unit_test::data::xrange(0, 11), var)
+    {
+
+        // Unit conversion - from m to ft
+        double crestElev_var = crestElev[var] / .3048;       
+        double nodeHead_var = nodeHead[var] / .3048;       
+        double overlandHead_var = overlandHead[var] / .3048;
+        double weirWidth_var = weirWidth[var] / .3048;
+        double overflowArea_var = overflowArea[var] / (.3048 * .3048);
+
+        couplingType = opening_findCouplingType(crestElev[var], nodeHead[var], overlandHead[var],
+                                                overflowArea[var], weirWidth[var]);
+        BOOST_CHECK_EQUAL(couplingType, ExpectedType[var]);
+
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
