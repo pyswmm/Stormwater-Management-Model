6401981885f18110eb746ee6f2693ad36509a753
Laurent Courty <lrntct@gmail.com>
2020-06-17 15:53:47 -0500

    edit linux whl tests

--- tools/build-wheels.sh
+++ tools/build-wheels.sh
@@ -26,6 +26,6 @@ done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+    "${PYBIN}/pip" install swmm --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/pytest")
 done
