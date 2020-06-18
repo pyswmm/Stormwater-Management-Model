f98746414075c29e78988316b91a19054373464a
Laurent Courty <lrntct@gmail.com>
2020-06-18 00:00:09 -0500

    edit build_wheels.ps1

--- tools/build_wheels.ps1
+++ tools/build_wheels.ps1
@@ -1,5 +1,6 @@
-foreach ($PYBIN in C:\Python3?-x64\\python.exe) {
-    Set-Location -Path $env:APPVEYOR_BUILD_FOLDER
+Set-Location -Path $env:APPVEYOR_BUILD_FOLDER
+$files = Get-ChildItem -Path C:\Python3?-x64\* -Include python.exe
+foreach ($PYBIN in $files) {
     & $PYBIN -m pip install --upgrade pip
     & $PYBIN -m pip install scikit-build
     & $PYBIN -m pip wheel . --no-deps -w dist
