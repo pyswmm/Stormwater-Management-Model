c4b6b1025f0ed904a2e31f4938d2a9b49e1f99a7
Laurent Courty <lrntct@gmail.com>
2020-06-18 00:03:56 -0500

    remove appveyor python matrix

--- tools/build_wheels.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location -Path $env:APPVEYOR_BUILD_FOLDER
-$files = Get-ChildItem -Path C:\Python3?-x64\* -Include python.exe
-foreach ($PYBIN in $files) {
-    & $PYBIN -m pip install --upgrade pip
-    & $PYBIN -m pip install scikit-build
-    & $PYBIN -m pip wheel . --no-deps -w dist
-}
