da80241f6bf0d76de5a24697bc54b276c5940ac8
Laurent Courty <lrntct@gmail.com>
2020-06-17 20:14:33 -0500

    manylinux: edit whl test

--- tools/build-wheels.sh
+++ tools/build-wheels.sh
@@ -15,7 +15,6 @@ function repair_wheel {
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    echo "${PYBIN}"
     "${PYBIN}/pip" install scikit-build
     "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
 done
@@ -28,6 +27,5 @@ done
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
     "${PYBIN}/pip" install swmm --no-index -f /io/wheelhouse --prefix .
-    "${PYBIN}/pip" show -f swmm
     (cd "$HOME"; "./bin/run-swmm --version")
 done
